a975d2108b36f9197a00ba250018702a4079adc5
Dhcdev <53800325+Dhcdev@users.noreply.github.com>
2020-04-22 22:26:48 -0300

    3commit

--- src/pages/create-account.js
+++ src/pages/create-account.js
@@ -1,95 +1,168 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React, { useState } from "react"
+import Layout from "../components/Layout"
+import firebase from '../lib/firebase'
+
 const CreateAccount = () => {
+    const [form, setForm] = useState({
+        email: 'davidhumbertocosta@gmail.com',
+        passwd: '123456',
+        passwd2: '123456'
+    })
+
+    const onChange = evt => {
+        const value = evt.target.value
+        const field = evt.target.name   
+        setForm(oldForm => ({
+            ...oldForm,
+            [field]: value
+        }))
+    }
+    const createAccount = () => {
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword('david', '123')
+        .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode, errorMessage)
+        // ...
+    });
+
+    }
+
+        const changePasswd = evt => {
+            const value = evt.target.value
+            setForm(oldForm => ({
+                ...oldForm,
+                passwd: value
+            }))
+
+    }
     return (
-        <Layout>
+        <Layout>   
+        
 
-            <h1>Create account</h1>
-
-
-            <div class="container max-w-full mx-auto md:py-24 px-6">
-                <div class="max-w-sm mx-auto px-6">
-                    <div class="relative flex flex-wrap">
-                        <div class="w-full relative">
-                            <div class="md:mt-6">
-                                <div class="text-center font-semibold text-black">
+           <div className="container max-w-full mx-auto md:py-24 px-6">
+                <div className="max-w-sm mx-auto px-6">
+                    <div className="relative flex flex-wrap">
+                        <div className="w-full relative">
+                            <div className="md:mt-6">
+                                <div className="text-center font-semibold text-black">
                                     Lorem ipsum dolor
-                            </div>
-                                <div class="text-center font-base text-black">
-                                    Sed ut perspiciatis unde?
                                 </div>
-                                <form class="mt-8" x-data="{password: '',password_confirm: ''}">
-                                    <div class="mx-auto max-w-lg ">
-                                        <div class="py-1">
-                                            <span class="px-1 text-sm text-gray-600">Username</span>
-                                            <input placeholder="" type="text" class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
+                                    <div className="text-center font-base text-black">
+                                    Sed ut perspiciatis unde?
+                                    </div>
+                    <form className="mt-8" x-data="{password: '',password_confirm: ''}">
+                                       <div className="mx-auto max-w-lg ">
+                                       <div className="py-1">
+                                            <span className="px-1 text-sm text-gray-600">Email</span>
+                                            <input 
+                                            value={form.email}
+                                            name='email'
+                                            onChange={onChange} 
+                                            placeholder="" 
+                                            type="email" 
+                                            className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
                                         </div>
-                                        <div class="py-1">
-                                            <span class="px-1 text-sm text-gray-600">Email</span>
-                                            <input placeholder="" type="email" class="text-md block px-3 py-2 rounded-bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
+                                        <div className="py-1">
+                                            <span className="px-1 text-sm text-gray-600">Password</span>
+                                            <input 
+                                            placeholder="" 
+                                            value={form.passwd}
+                                            onChange={onChange} 
+                                            name='passwd'
+                                            type="password" x-model="password" 
+                                            className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
                                         </div>
-                                        <div class="py-1">
-                                            <span class="px-1 text-sm text-gray-600">Password</span>
-                                            <input placeholder="" type="password" x-model="password" class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
+                                        <div className="py-1">
+                                            <span className="px-1 text-sm text-gray-600">Password Confirm</span>
+                                            <input 
+                                            placeholder="" 
+                                            value={form.passwd2}
+                                            onChange={onChange}
+                                            name='passwd2'
+                                            type="password" x-model="password_confirm"
+                                            className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
                                         </div>
-                                        <div class="py-1">
-                                            <span class="px-1 text-sm text-gray-600">Password Confirm</span>
-                                            <input placeholder="" type="password" x-model="password_confirm" class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
-                                        </div>
-                                        <div class="flex justify-start mt-3 ml-4 p-1">
+                                        <div className="flex justify-start mt-3 ml-4 p-1">
                                             <ul>
-                                                <li class="flex items-center py-1">
-                                                    <div class="{'bg-green-200 text-green-700': password == password_confirm && password.length > 0, 'bg-red-200 text-red-700':password != password_confirm || password.length == 0}" class=" rounded-full p-1 fill-current ">
-                                                        <svg class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <li className="flex items-center py-1">
+                                                    <div className="{'bg-green-200 text-green-700': password == password_confirm && password.length > 0, 'bg-red-200 text-red-700':password != password_confirm || password.length == 0}" className=" rounded-full p-1 fill-current ">
+                                                        <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                             <path x-show="password == password_confirm && password.length > 0" stroke-linecap="round"
                                                                 stroke-linejoin="round" stroke-width="2"
                                                                 d="M5 13l4 4L19 7" />
-                                                            <path x-show="password != password_confirm || password.length == 0" stroke-linecap="round"
-                                                                stroke-linejoin="round" stroke-width="2"
-                                                                d="M6 18L18 6M6 6l12 12" />
+                                                            <path 
+                                                            x-show="password != password_confirm || password.length == 0" 
+                                                            stroke-linecap="round"
+                                                            stroke-linejoin="round" 
+                                                            stroke-width="2"
+                                                            d="M6 18L18 6M6 6l12 12" />
                                                         </svg>
                                                     </div>
-                                                    <span class="{'text-green-700': password == password_confirm && password.length > 0, 'text-red-700':password != password_confirm || password.length == 0}" class="font-medium text-sm ml-3" x-text="password == password_confirm && password.length > 0 ? 'Passwords match' : 'Passwords do not match' "></span>
-                                                </li>
-                                                <li class="flex items-center py-1">
-                                                    <div class="{'bg-green-200 text-green-700': password.length > 7, 'bg-red-200 text-red-700':password.length < 7 }" class=" rounded-full p-1 fill-current ">
-                                                        <svg class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                            <path x-show="password.length > 7" stroke-linecap="round"
-                                                                stroke-linejoin="round" stroke-width="2"
-                                                                d="M5 13l4 4L19 7" />
-                                                            <path x-show="password.length < 7" stroke-linecap="round"
-                                                                stroke-linejoin="round" stroke-width="2"
-                                                                d="M6 18L18 6M6 6l12 12" />
+                                                    <span 
+                                                    className="{'text-green-700': password == password_confirm && password.length > 0, 'text-red-700':password != password_confirm || password.length == 0}" 
+                                                    className="font-medium text-sm ml-3" >
+                                                    
+                                                    { form.passwd === form.passwd2 && 'Passwords match' }
+                                                    { form.passwd !== form.passwd2 && 'Passwords do not match' }
+                                                    
+                                                    </span>
 
+                                                    </li>
+                                                <li className="flex items-center py-1">
+                                                    <div className="{'bg-green-200 text-green-700': password.length > 7, 'bg-red-200 text-red-700':password.length < 7 }" className=" rounded-full p-1 fill-current ">
+                                                        <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path 
+                                                            x-show="password.length > 7" 
+                                                            stroke-linecap="round"
+                                                            stroke-linejoin="round" 
+                                                            stroke-width="2"
+                                                            d="M5 13l4 4L19 7" 
+                                                            />
+                                                            <path 
+                                                            x-show="password.length < 7" 
+                                                            stroke-linecap="round"
+                                                            stroke-linejoin="round" 
+                                                            stroke-width="2"
+                                                            d="M6 18L18 6M6 6l12 12" 
+                                                            />
                                                         </svg>
                                                     </div>
-                                                    <span class="{'text-green-700': password.length > 7, 'text-red-700':password.length < 7 }" class="font-medium text-sm ml-3" x-text="password.length > 7 ? 'The minimum length is reached' : 'At least 8 characters required' "></span>
+                                                    <span 
+                                                    className="{'text-green-700': password.length > 7, 'text-red-700':password.length < 7 }" 
+                                                    className="font-medium text-sm ml-3" 
+                                                    x-text="password.length > 7 ? 'The minimum length is reached' : 'At least 8 characters required' ">
+                                                   
+                                                    </span>
                                                 </li>
                                             </ul>
                                         </div>
-                                        <div class="flex justify-start">
-                                            <label class="block text-gray-500 font-bold my-4 flex items-center">
-                                                <input class="leading-loose text-pink-600 top-0" type="checkbox" />
-                                                <span class="ml-2 text-sm py-2 text-gray-600 text-left">Accept the
-                                                   <a href="#" class="font-semibold text-black border-b-2 border-gray-200 hover:border-gray-500">
+                                        <div className="flex justify-start">
+                                            <label className="block text-gray-500 font-bold my-4 flex items-center">
+                                                <input className="leading-loose text-pink-600 top-0" type="checkbox" />
+                                                <span className="ml-2 text-sm py-2 text-gray-600 text-left">Accept the
+                                                   <a href="#" className="font-semibold text-black border-b-2 border-gray-200 hover:border-gray-500">
                                                         Terms and Conditions of the site
                                                    </a>and
-                                                   <a href="#" class="font-semibold text-black border-b-2 border-gray-200 hover:border-gray-500">
+                                                   <a href="#" className="font-semibold text-black border-b-2 border-gray-200 hover:border-gray-500">
                                                         the information data policy.</a>
                                                 </span>
                                             </label>
                                         </div>
-                                        <button class="mt-3 text-lg font-semibold bg-gray-800 w-full text-white rounded-lg px-6 py-3 block shadow-xl hover:text-white hover:bg-black">
+                                        <button type='button' onClick={createAccount} className="mt-3 text-lg font-semibold bg-gray-800 w-full text-white rounded-lg px-6 py-3 block shadow-xl hover:text-white hover:bg-black">
                                             Register
                                      </button>
                                     </div>
                                 </form>
 
-                                <div class="text-sm font-semibold block sm:hidden py-6 flex justify-center">
-                                    <a href="#" class="text-black font-normal border-b-2 border-gray-200 hover:border-teal-500">You're already member?
+                                <div className="text-sm font-semibold block sm:hidden py-6 flex justify-center">
+                                    <a href="#" className="text-black font-normal border-b-2 border-gray-200 hover:border-teal-500">You're already member?
 
-                   <span class="text-black font-semibold">
-                                            Login
+                   <span className="text-black font-semibold">
+                     Login
                    </span>
                                     </a>
                                 </div>
